651d5af4c277b2a28852d7461f3df9ae43b01b40
Artemon <artemijna@gmail.com>
2023-01-24 15:43:52 +0300

    i corect project

--- password_generaton/settings.py
+++ password_generaton/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f)obm3(959$9cr)pyiu_i%$wfx#0^$ngtkfq_rd4q$yox=t=&b'
+SECRET_KEY = 'django-insecure-f)obm3(959$9cr)pyiu_i%$wfx#0^$ngtkfq_rd4q$yoxxx=t=&b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
